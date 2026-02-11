47e5b011b5645aa41f30af4e1887540e5f19285e
alaina <asrivastav2@wisc.edu>
2026-02-11 09:30:17 -0600

    added blog post

--- src/content/posts.ts
+++ src/content/posts.ts
@@ -1,17 +1,24 @@
 import AIBuzzword from './blog/AI_ tlb.md';
 import NinePerfectStrangers from './blog/Nine Perfect Strangers versus White Lotus.md';
+import AIRelationshipsBrain from './blog/AI, relationships, brain.md';
 
 export const posts = {
   'ai-latest-buzzword': {
     component: AIBuzzword,
     title: 'AI: the Latest Buzzword',
     date: '2/23/24',
-    excerpt: 'AI is the new big thing...'
+    excerpt: 'AI is the new big thing. Let\'s talk about it.'
   },
   'nine-perfect-strangers-vs-white-lotus': {
     component: NinePerfectStrangers,
     title: 'Nine Perfect Strangers versus The White Lotus',
     date: '5/30/25',
-    excerpt: 'They are actually quite different!'
+    excerpt: 'My thoughts on two mind-boggling shows.'
+  },
+  'ai-relationships-brain': {
+    component: AIRelationshipsBrain,
+    title: 'What AI is Doing to Our Brains and Our Relationships',
+    date: '2/8/26',
+    excerpt: 'What does preliminary research tell us?'
   }
 };
